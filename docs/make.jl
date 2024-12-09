30eae24e68a47a7ff7bfe04c332d73ee54b6c794
MilesCranmer <miles.cranmer@gmail.com>
2024-12-09 01:47:28 +0000

    docs: separate API page

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,41 @@
 using DynamicDiff
 using Documenter
 
+readme = joinpath(@__DIR__, "..", "README.md")
+index_content = let r = read(readme, String)
+    # Wrap img tags in raw HTML blocks:
+    r = replace(r, r"(<img\s+[^>]+>)" => s"""
+
+```@raw html
+\1
+```
+
+""")
+    # Remove end img tags:
+    r = replace(r, r"</img>" => "")
+    # Remove div tags:
+    r = replace(r, r"<div[^>]*>" => "")
+    # Remove end div tags:
+    r = replace(r, r"</div>" => "")
+
+    top_part = """
+    # Introduction
+
+    """
+
+    bottom_part = """
+    ## Contents
+
+    """
+
+    join((top_part, r, bottom_part), "\n")
+end
+
+index_md = joinpath(@__DIR__, "src", "index.md")
+open(index_md, "w") do f
+    write(f, index_content)
+end
+
 DocMeta.setdocmeta!(DynamicDiff, :DocTestSetup, :(using DynamicDiff); recursive=true)
 
 makedocs(;
@@ -13,7 +48,7 @@ makedocs(;
         edit_link="main",
         assets=String[],
     ),
-    pages=["Home" => "index.md"],
+    pages=["Home" => "index.md", "API" => "api.md"],
 )
 
 ENV["GITHUB_REPOSITORY"] = "ai-damtp-cam-ac-uk/dynamicdiff.git"
