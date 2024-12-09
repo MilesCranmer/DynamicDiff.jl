5c7991fe94535453a0058ab6843e36a39eb10d65
MilesCranmer <miles.cranmer@gmail.com>
2024-12-09 00:56:17 +0000

    docs: push to damtp URL

--- docs/make.jl
+++ docs/make.jl
@@ -8,11 +8,16 @@ makedocs(;
     authors="MilesCranmer <miles.cranmer@gmail.com> and contributors",
     sitename="DynamicDiff.jl",
     format=Documenter.HTML(;
-        canonical="https://MilesCranmer.github.io/DynamicDiff.jl",
+        canonical="https://ai.damtp.cam.ac.uk/dynamicdiff/stable",
         edit_link="main",
         assets=String[],
     ),
     pages=["Home" => "index.md"],
 )
 
-deploydocs(; repo="github.com/MilesCranmer/DynamicDiff.jl", devbranch="main")
+deploydocs(; repo="github.com/MilesCranmer/DynamicDiff.jl")
+
+# Mirror to DAMTP
+ENV["DOCUMENTER_KEY"] = ENV["DOCUMENTER_KEY_CAM"]
+ENV["GITHUB_REPOSITORY"] = "ai-damtp-cam-ac-uk/dynamicdiff.git"
+deploydocs(; repo="github.com/ai-damtp-cam-ac-uk/dynamicdiff.git")
